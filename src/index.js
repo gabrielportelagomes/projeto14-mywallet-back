e091590b95080f20b55454cae14696865850900d
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-16 16:04:01 -0300

    feat:POST request on route /sign-up

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import bcrypt from "bcrypt";
 import { v4 as uuidV4 } from "uuid";
 import { stripHtml } from "string-strip-html";
 
+const userSchema = joi.object({
+  name: joi.string().required().min(3).max(100),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
+
 const app = express();
 
 dotenv.config();
@@ -19,12 +25,39 @@ let db;
 
 try {
   await mongoClient.connect();
+  db = mongoClient.db("myWallet");
   console.log("MongoDb Conectado");
 } catch (err) {
   console.log("Erro ao conectar com o MongoDb: ", err.message);
 }
 
 db = mongoClient.db("myWallet");
+const usersCollection = db.collection("users");
+
+app.post("/sign-up", async (req, res) => {
+  const user = req.body;
+
+  try {
+    const registeredUser = await usersCollection.findOne({ email: user.email });
+    if (registeredUser) {
+      return res.status(409).send({ message: "E-mail já cadastrado!" });
+    }
+
+    const { error } = userSchema.validate(user, { abortEarly: false });
+
+    if (error) {
+      const errorDetails = error.details.map((detail) => detail.message);
+      return res.status(400).send(errorDetails);
+    }
+
+    const hashPassword = bcrypt.hashSync(user.password, 12);
+    await usersCollection.insertOne({ ...user, password: hashPassword });
+    res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
